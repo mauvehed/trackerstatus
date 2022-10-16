68986f4af6cb92a506a752107f81bc840aadbffa
mauvehed <mh@mvh.dev>
2022-10-16 00:58:49 +0000

    Clean up, get_site_api and get_sites_all_api

--- trackerstatus/api.py
+++ trackerstatus/api.py
@@ -4,48 +4,40 @@ import requests
 API_ALL_SITES_URL = 'https://trackerstatus.info/api/list/'
 
 class Trackerstatus:
-    '''Class for the trackerstatus command'''
-    def __init__(self):
-        '''Initialize the class'''
-        self.data = ''
+    '''Fetches data from the trackerstatus.info API
 
-    def get_allsites_api_all(self):
+    Args:
+        site_name (str): The site name to fetch data for
+                         Must match 'all', 'ar', 'btn', 'ggn', 'ptp', 'red', or 'ops'
+
+    Returns:
+        dict: The data from the API
+    '''
+    def __init__(self, site_name):
+        '''Initialize the class'''
+        self.site_name = site_name
+        self.data = ''
+        if site_name == 'all':
+            self.get_sites_all_api()
+
+    def get_site_api(self, api_type):
+        '''Get the API data
+
+        api_type can be one of the following:
+        - status
+        - latency
+        - uptime
+        - records
+        - downtime
+        - all
+        '''
+        api_site_url = f'https://{self.site_name}.trackerstatus.info/api/{api_type}/'
+        response_is = requests.get(api_site_url,timeout=15)
+        if response_is.status_code == 200:
+            self.data = response_is.json()
+        return self.data
+
+    def get_sites_all_api(self):
         '''Get the top level /api/all/ sites API data'''
         self.data = requests.get(API_ALL_SITES_URL,timeout=15).json()
-        return self.data
-
-    def get_site_api_all(self, site_name):
-        '''Get the site specific /api/all/ API data'''
-        api_site_all_url = f'https://{site_name}.trackerstatus.info/api/all/'
-        self.data = requests.get(api_site_all_url,timeout=15).json()
-        return self.data
-
-    def get_site_api_status(self, site_name):
-        '''Get the site specific /api/status/ API data'''
-        api_site_status_url = f'https://{site_name}.trackerstatus.info/api/status/'
-        self.data = requests.get(api_site_status_url,timeout=15).json()
-        return self.data
-
-    def get_site_api_latency(self, site_name):
-        '''Get the site specific /api/latency/ API data'''
-        api_site_latency_url = f'https://{site_name}.trackerstatus.info/api/latency/'
-        self.data = requests.get(api_site_latency_url,timeout=15).json()
-        return self.data
-
-    def get_site_api_uptime(self, site_name):
-        '''Get the site specific /api/uptime/ API data'''
-        api_site_uptime_url = f'https://{site_name}.trackerstatus.info/api/uptime/'
-        self.data = requests.get(api_site_uptime_url,timeout=15).json()
-        return self.data
-
-    def get_site_api_records(self, site_name):
-        '''Get the site specific /api/records/ API data'''
-        api_site_records_url = f'https://{site_name}.trackerstatus.info/api/records/'
-        self.data = requests.get(api_site_records_url,timeout=15).json()
-        return self.data
-
-    def get_site_api_downtime(self, site_name):
-        '''Get the site specific /api/downtime/ API data'''
-        api_site_downtime_url = f'https://{site_name}.trackerstatus.info/api/downtime/'
-        self.data = requests.get(api_site_downtime_url,timeout=15).json()
         return self.data
